cc533e43e30944fdcd161fd926326fb15da7593e
divyapatel8401@gmail.com <divyapatel8401@gmail.com>
2016-12-18 22:53:19 -0600

    import java files (delete multiple records)

---
+++ SingleLinkedList/Link.java
@@ -0,0 +1,13 @@
+package SingleLinkedList;
+public class Link {
+	public Link next;
+	public int data;
+	
+	public Link(int data){
+		this.data = data;
+	}
+	
+	public int data(){
+		return data;
+	}
+}
---
+++ SingleLinkedList/SingleLinkedList.java
@@ -0,0 +1,126 @@
+package SingleLinkedList;
+public class SingleLinkedList {
+	private Link head;
+	
+	public SingleLinkedList(){
+		head = null;
+	}
+	
+	public void insertData(int data){
+		Link newNode = new Link(data);
+		newNode.next = head;
+		head = newNode;
+	}
+	
+	public void print(){
+		if(head == null){
+			System.out.println("No Data Inserted");
+		} else {
+			Link tempNode = head;
+			while(tempNode != null){
+				System.out.println(tempNode.data());
+				tempNode = tempNode.next;
+			}
+		}
+	}
+	
+	public void delete(){
+		if(head == null){
+			System.out.println("Cant delete the data");
+		} else {
+			head = head.next;
+		}
+	}
+	
+	public void deleteData(int data){
+		if(head == null){
+			System.out.println("No data in linked list");
+		}
+		else if(head.data() == data){
+			head = head.next;
+		}else {
+			Link tempLink = head;
+			while(tempLink.next.data() != data){
+				tempLink = tempLink.next;
+			}
+			Link newNode = tempLink.next;
+			tempLink.next = newNode.next;
+		}
+	}
+	public void insertAsIWant(int node, int data){
+		if(head == null){
+			System.out.println("No data in linked list");
+			//return 0;
+		}
+		else{
+			Link tempLink = head;
+			while(tempLink.data() != node ){
+				tempLink = tempLink.next;
+			}
+			Link newNode = new Link(data);
+			newNode.next = tempLink.next;
+			tempLink.next = newNode;
+		}	
+		
+	}
+	public void updateValue(int node, int value){
+		if(head == null){
+			System.out.println("No data in linked list");
+			//return 0;
+		}
+		else{
+			Link tempLink = head;
+			while(tempLink.data() != node ){
+				tempLink = tempLink.next;
+			}
+			//Link newNode = new Link(value);
+			tempLink.data = value;
+		}
+	}
+	
+	public void deleteMultipleData(int data){
+		Link ptr = head;
+		while(ptr != null){
+			if(ptr.data() == data){
+				if(ptr == head){
+					ptr = ptr.next;
+					head = head.next;
+					
+				}
+				
+			}
+			else if(ptr.next != null){
+				
+				if(ptr.next.data() == data){
+					
+					ptr.next = ptr.next.next;
+					ptr = ptr.next;
+				}	
+				else{
+					ptr = ptr.next;
+				}
+			
+		
+			}
+			else{
+				ptr = null;
+			}
+			
+		}
+	}
+	
+	public static void main(String args[]){
+		SingleLinkedList linkedList = new SingleLinkedList();
+		linkedList.insertData(5);
+		linkedList.insertData(10);
+		linkedList.insertData(7);
+		linkedList.insertData(15);
+		linkedList.deleteData(15);
+		linkedList.insertAsIWant(7, 20);
+		linkedList.insertAsIWant(5, 30);
+		linkedList.updateValue(5, 40);
+		linkedList.updateValue(10, 7);
+		linkedList.deleteMultipleData(7);
+		linkedList.print();
+	}
+}
